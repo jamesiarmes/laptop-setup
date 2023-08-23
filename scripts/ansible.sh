18685404ea899c524f5c450525dcf72e9e86ea75
James Armes <jarmes@codeforamerica.org>
2023-08-23 11:59:56 -0400

    Added work inventory.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEXT_COLOR="\033[1;34m"
 NO_COLOR="\033[0m"
+INVENTORY="${ANSIBLE_INVENTORY:-home}"
 
 echo -e "${TEXT_COLOR}Installing python dependencies${NO_COLOR}"
 poetry install
@@ -10,7 +11,7 @@ poetry install
 echo -e "${TEXT_COLOR}Installing ansible dependencies${NO_COLOR}"
 poetry run ansible-galaxy install -r "$SCRIPT_DIR/../ansible/requirements.yml"
 
-echo -e "${TEXT_COLOR}Executing ansible playbook${NO_COLOR}"
-poetry run ansible-playbook "$SCRIPT_DIR/../ansible/setup.yml" --ask-become-pass -i "$SCRIPT_DIR/../ansible/inventory/home.yml"
+echo -e "${TEXT_COLOR}Executing ansible playbook for ${INVENTORY}${NO_COLOR}"
+poetry run ansible-playbook "$SCRIPT_DIR/../ansible/setup.yml" --ask-become-pass -i "$SCRIPT_DIR/../ansible/inventory/$INVENTORY.yml"
 
 echo -e "${TEXT_COLOR}Setup complete${NO_COLOR}"
